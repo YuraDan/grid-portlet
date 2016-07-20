9b4cbb3332532c26d34bb63df28659f16a77d3ad
Yuri <burov.y@sovzond.ru>
2016-07-20 17:07:27 +0300

    Grid portlet service works with liferay identification.

--- src/main/webapp/js/modules/dataView.js
+++ src/main/webapp/js/modules/dataView.js
@@ -1,149 +1,158 @@
 (function () {
-		var hostPath = window.location.protocol + '//' + window.location.host + '/',
-			colors = [
-				'#7cf778',
-				'#f80000',
-				'#4fd7e4',
-				'#5440e6',
-				'#ff3ff9',
-				'transparent',
-				'#f1bf1a',
-				'#ff8a5c',
-				'#3f8cff',
-				'#05924e',
-				'#a70e0e'
-			],
-			extend = angular.extend,
-			initialData = [],
-			setDataToTable = function (table, data) {
-				table.option('dataSource', data);
-				table.refresh();
-			};
+	var hostPath = window.location.protocol + '//' + window.location.host + '/',
+		colors = [
+			'#7cf778',
+			'#f80000',
+			'#4fd7e4',
+			'#5440e6',
+			'#ff3ff9',
+			'transparent',
+			'#f1bf1a',
+			'#ff8a5c',
+			'#3f8cff',
+			'#05924e',
+			'#a70e0e'
+		],
+		extend = angular.extend,
+		initialData = [],
+		setDataToTable = function (table, data) {
+			table.option('dataSource', data);
+			table.refresh();
+		};
 
-		angular.module('dataView', [])
-			.factory('gridConfig', ['$http', function ($http) {
-				return function (componentId, successCallback, failCallback) {
-					$http.get(hostPath + 'delegate/services/gridConfig/', {params: {componentId: componentId}}).then(successCallback, failCallback || angular.noop);
-				}
-			}])
-			.factory('gridData', ['$http', function ($http) {
-				return function (url, dataSetName, successCallback, failCallback) {
-					$http.get(url, {params: {dataSetName: dataSetName}}).then(successCallback, failCallback || angular.noop);
-				}
-			}])
-			.component('grDataView', {
-				transclude: false,
-				templateUrl: hostPath + 'liferay-grid-portlet/html/dataView.html',
-				controller: ['$scope', '$element', 'gridConfig', 'gridData', function ($scope, element, gridConfig, gridData) {
-					var table,
-						selectedRowIndex = -1,
-						currentRow,
-						iDataView;
+	angular.module('dataView', [])
+		.factory('gridConfig', ['$http', function ($http) {
+			return function (portletId, userId, plId, successCallback, failCallback) {
+				$http.get(hostPath + 'delegate/services/gridConfig/', { params: { portletId: portletId, userId: userId, plId: plId } }).then(successCallback, failCallback || angular.noop);
+			}
+		}])
+		.factory('gridData', ['$http', function ($http) {
+			return function (url, dataSetName, userId, param, successCallback, failCallback) {
+				$http.get(url, { params: { dataSetName: dataSetName, userId: userId, param: param } }).then(successCallback, failCallback || angular.noop);
+			}
+		}])
+		.component('grDataView', {
+			transclude: false,
+			templateUrl: hostPath + 'liferay-grid-portlet/html/dataView.html',
+			controller: ['$scope', '$element', 'gridConfig', 'gridData', function ($scope, element, gridConfig, gridData) {
+				var table,
+					selectedRowIndex = -1,
+					currentRow,
+					iDataView;
 
-					//--- Контекст ---
-					extend($scope, {
-						settings: {table: {}},
-						data: []
-					});
+				//--- Контекст ---
+				extend($scope, {
+					settings: { table: {} },
+					data: []
+				});
 
-					//--- Настройки таблицы ---
-					extend($scope.settings.table, {
-						bindingOptions: {
-							dataSource: 'data'
-						},
-						//--- Выбор строки ---
-						onRowClick: function (e) {
-							if (e.rowType === 'data') {
-								//--- Выделение строки ---
-								currentRow && currentRow.removeClass('current-row');
-								currentRow = e.rowElement;
-								currentRow.addClass('current-row');
+				//--- Настройки таблицы ---
+				extend($scope.settings.table, {
+					bindingOptions: {
+						dataSource: 'data'
+					},
+					//--- Выбор строки ---
+					onRowClick: function (e) {
+						if (e.rowType === 'data') {
+							//--- Выделение строки ---
+							currentRow && currentRow.removeClass('current-row');
+							currentRow = e.rowElement;
+							currentRow.addClass('current-row');
 
-								//--- Запоминаем для двойного клика выбранную строку ---
-								selectedRowIndex = e.rowIndex;
-							}
-							else {
-								//--- Запоминаем для двойного клика выбранную строку ---
-								selectedRowIndex = -1;
-							}
-						},
-						//--- Выбор атрибута ---
-						onCellClick: function (e) {
-							//--- Событие ---
-							//iDataView.onAttributeSelect(e.rowIndex, e.column.dataField);
-						},
-						//--- Выделение строк ---
-						onSelectionChanged: function (e) {
-							var rowIndexes = [],
-								selectedData = e.selectedRowsData,
-								data = $scope.data,
-								dataRow,
-								i,
-								q;
+							//--- Запоминаем для двойного клика выбранную строку ---
+							selectedRowIndex = e.rowIndex;
+						}
+						else {
+							//--- Запоминаем для двойного клика выбранную строку ---
+							selectedRowIndex = -1;
+						}
+					},
+					//--- Выбор атрибута ---
+					onCellClick: function (e) {
+						//--- Событие ---
+						//iDataView.onAttributeSelect(e.rowIndex, e.column.dataField);
+					},
+					//--- Выделение строк ---
+					onSelectionChanged: function (e) {
+						var rowIndexes = [],
+							selectedData = e.selectedRowsData,
+							data = $scope.data,
+							dataRow,
+							i,
+							q;
 
-							for (i = data.length; i--;) {
-								dataRow = data[i];
-								for (q = selectedData.length; q--;) {
-									if (selectedData[q] === dataRow) {
-										rowIndexes.push(i);
-										selectedData.splice(q, 1);
-										break;
-									}
-								}
-								if (selectedData.length === 0) {
+						for (i = data.length; i--;) {
+							dataRow = data[i];
+							for (q = selectedData.length; q--;) {
+								if (selectedData[q] === dataRow) {
+									rowIndexes.push(i);
+									selectedData.splice(q, 1);
 									break;
 								}
 							}
-
-							//--- Событие ---
-							//iDataView.onSelectRows(rowIndexes);
-						},
-						//--- Таблица проинициализирована ---
-						onInitialized: function (e) {
-							table = e.component;
-							$scope.data = initialData || [];
+							if (selectedData.length === 0) {
+								break;
+							}
 						}
-					});
 
-					//--- Строка выбрана для редактирования ---
-					element.on('dxdblclick', function () {
-						//--- Снимаем выделение по умолчанию ---
-						if (window.getSelection) {
-							window.getSelection().removeAllRanges();
-						} else { // старый IE
-							document.selection.empty();
-						}
 						//--- Событие ---
-						//iDataView.onRowSelect(selectedRowIndex);
-					});
+						//iDataView.onSelectRows(rowIndexes);
+					},
+					//--- Таблица проинициализирована ---
+					onInitialized: function (e) {
+						table = e.component;
+						$scope.data = initialData || [];
+					}
+				});
 
-					//--- Запрос настроек таблицы ---
-					gridConfig(
-						1, //componentId - идентификатор компонента для которого запрашиваются настройки
-						function (response) {
-							var gridOptions = response.data.dxDataGrid,
-								dataSetName = response.data.dataSource.name,
-								dataUrl = hostPath + 'delegate/services/gridData';
-							table.option(gridOptions);
-							table.repaint();
+				//--- Строка выбрана для редактирования ---
+				element.on('dxdblclick', function () {
+					//--- Снимаем выделение по умолчанию ---
+					if (window.getSelection) {
+						window.getSelection().removeAllRanges();
+					} else { // старый IE
+						document.selection.empty();
+					}
+					//--- Событие ---
+					//iDataView.onRowSelect(selectedRowIndex);
+				});
 
-							//--- Запрос данных таблицы ---
-							gridData(
-								dataUrl,
-								dataSetName,
-								function (response) {
-									table.option('dataSource', response.data);
-									table.refresh();
-								},
-								function (response) {
-									console.error('Ошибка при получении данных');
-								}
-							);
-						},
-						function (response) {
-							console.error('Ошибка при получении настроек таблицы');
-						}
-					);
-				}]
-			});
-	})();
+				//--- Запрос настроек таблицы ---
+				var portletId = element.parents('section.portlet').attr('id'),
+					userId = parseInt(Liferay.ThemeDisplay.getUserId()),
+					plId = parseInt(Liferay.ThemeDisplay.getPlid());
+
+				gridConfig(
+					portletId,
+					userId,
+					plId,
+					function (response) {
+						var gridOptions = response.data.dxDataGrid,
+							dataSetName = response.data.dataSource.name,
+							dataUrl = hostPath + 'delegate/services/gridData',
+							param = "{}";
+						table.option(gridOptions);
+						table.repaint();
+
+						//--- Запрос данных таблицы ---
+						gridData(
+							dataUrl,
+							dataSetName,
+							userId,
+							param,
+							function (response) {
+								table.option('dataSource', response.data);
+								table.refresh();
+							},
+							function (response) {
+								console.error('Ошибка при получении данных');
+							}
+						);
+					},
+					function (response) {
+						console.error('Ошибка при получении настроек таблицы');
+					}
+				);				
+			}]
+		});
+})();
